91c507d496f51a35e0d9b232c3d1bcedbf5186d7
Shaw Hsu <scorrxu@gmail.com>
2013-05-05 15:50:34 +0800

    完善console展现面板的能力

--- src/plugins/viewport.js
+++ src/plugins/viewport.js
@@ -4,11 +4,11 @@
 		, DOMID_ANCHOR = '___cp_console_viewport_anchor'
 		, APJ = Array.prototype.join
 		, LOG_MAP = {
-			debug:			'<d> '
-			, error:		'<x> '
-			, info:			'<i> '
-			, log:			'<o> '
-			, warn:			'<!> '
+			debug:			'd> '
+			, error:		'x> '
+			, info:			'i> '
+			, log:			'o> '
+			, warn:			'!> '
 		}
 		, _wnd = window
 		, _doc = document
@@ -37,6 +37,32 @@
 			preventDefault(evt);
 		}
 
+		function clearViewportWindow(evt){
+			var d = _doc.getElementById(DOMID_PANEL)
+			, df = _doc.createDocumentFragment()
+			, l
+			;
+
+			if(d && d.getElementsByTagName){
+				l = d.getElementsByTagName('a');
+				if(l && l.length){
+					l = Array.prototype.slice.call(l);
+				}
+
+				function inception(ctn, list){
+					for(var i = 0, len = list.length; i < len; ++i){
+						ctn.appendChild(list[i]);
+					}
+				}
+
+				inception(df, l);
+				d.innerHTML = '';
+				inception(d, l);
+			}
+
+			preventDefault(evt);
+		}
+
 		function preventDefault(evt){
 			evt ? (evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false)) : 0;
 		}
@@ -53,6 +79,7 @@
 			var d
 			, a
 			, cb
+			, lb
 			;
 
 			if(a = _doc.getElementById(DOMID_ANCHOR)){
@@ -62,8 +89,8 @@
 				a.style.cssText = ([
 					'position:absolute;top:3px;right:3px;'
 					, 'border:solid 1px gray;'
-					, 'width:50px;height:16px;'
-					, 'font-family:verdana;text-decoration:none;font-size:12px;'
+					, 'width:52px;height:16px;'
+					, 'font-family:Consolas;text-decoration:none;font-size:12px;'
 					]).join('');
 				a.id = DOMID_ANCHOR;
 				a.innerHTML = 'Console';
@@ -76,6 +103,7 @@
 			if(d = _doc.getElementById(DOMID_PANEL)){
 				//存在就不干了
 			} else {
+				//面板
 				d = _doc.createElement('pre');
 				d.style.cssText = ([
 					'position:absolute;top:3px;right:3px;'
@@ -86,10 +114,12 @@
 					, 'background-color:rgba(255,255,255,0.5);'
 					, 'visibility:hidden;'
 					, 'overflow:scroll;'
+					, 'font-family:Consolas;font-size:12px;'
 					]).join('');
 				d.id = DOMID_PANEL;
 				_doc.body.appendChild(d);
 
+				//关闭按钮
 				cb = _doc.createElement('a');
 				cb.style.cssText = ([
 					'position:absolute;top:0;right:0;'
@@ -101,6 +131,19 @@
 				cb.title = 'Click to close';
 				d.appendChild(cb);
 				addEvent(cb, 'click', hideViewportWindow);
+
+				//清空按钮
+				lb = _doc.createElement('a');
+				lb.style.cssText = ([
+					'position:absolute;top:0;right:17px;'
+					, 'width:45px;height:15px;'
+					, 'font-family:Consolas;text-decoration:none;font-size:12px;'
+					]).join('');
+				lb.innerHTML = 'clear';
+				lb.href = 'javascript:;';
+				lb.title = 'Click to clear panel';
+				d.appendChild(lb);
+				addEvent(lb, 'click', clearViewportWindow);
 			}
 
 			refViewportPanelDomElem = d;
