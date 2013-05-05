89f97afff2785bd1be5550ceafeeb589d61c9e63
Shaw Hsu <scorrxu@gmail.com>
2013-05-05 14:38:31 +0800

    低端浏览器console panel展现完成

--- src/plugins/viewport.js
+++ src/plugins/viewport.js
@@ -1,29 +1,82 @@
 ('function' == typeof define) &&
 	define(function(require, exports, module){
 		var DOMID_PANEL = '___cp_console_viewport_panel'
+		, DOMID_ANCHOR = '___cp_console_viewport_anchor'
 		, proto
 		, _wnd = window
 		, _doc = document
 		;
 
-		//(dout = document.getElementById('cpvp_out')) && (dout.innerHTML = 'in module define -- viewport');
-		//window.console.log = function(){ alert('I\'m console-plus!'); };
+		function hideViewportWindow(evt){
+			var d = _doc.getElementById(DOMID_PANEL)
+			, a = _doc.getElementById(DOMID_ANCHOR)
+			;
+
+			d && (d.style.visibility = 'hidden');
+			a && (a.style.visibility = 'visible');
+
+			preventDefault(evt);
+		}
+
+		function showViewportWindow(evt){
+			var d = _doc.getElementById(DOMID_PANEL)
+			, a = _doc.getElementById(DOMID_ANCHOR)
+			;
+
+			d && (d.style.visibility = 'visible');
+			a && (a.style.visibility = 'hidden');
+
+			preventDefault(evt);
+		}
+
+		function preventDefault(evt){
+			evt ? (evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false)) : 0;
+		}
+
+		function addEvent(elem, type, fn){
+			if(elem.addEventListener){
+				elem.addEventListener(type, fn, false);
+			} else if(cb.attachEvent){
+				elem.attachEvent('on' + type, fn);
+			}
+		}
 
 		function createViewportWindow(){
 			var d
+			, a
 			, cb
 			;
 
+			if(a = _doc.getElementById(DOMID_ANCHOR)){
+				//存在就不干了
+			} else {
+				a = _doc.createElement('a');
+				a.style.cssText = ([
+					'position:absolute;top:3px;right:3px;'
+					, 'border:solid 1px gray;'
+					, 'width:50px;height:16px;'
+					, 'font-family:verdana;text-decoration:none;font-size:12px;'
+					]).join('');
+				a.id = DOMID_ANCHOR;
+				a.innerHTML = 'Console';
+				a.href = 'javascript:;';
+				a.title = 'Click to open console panel';
+				_doc.body.appendChild(a);
+				addEvent(a, 'click', showViewportWindow);
+			}
+
 			if(d = _doc.getElementById(DOMID_PANEL)){
+				//存在就不干了
 			} else {
 				d = _doc.createElement('pre');
 				d.style.cssText = ([
-					'position:absolute;top:3px;left:3px;'
+					'position:absolute;top:3px;right:3px;'
 					, 'border:solid 1px gray;'
-					, 'width:400px;height:200px;margin:0;'
+					, 'width:600px;height:300px;margin:0;'
 					, 'filter:progid:DXImageTransform.Microsoft.Alpha(opacity=50);'
 					, '-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(opacity=50)";'
-					, 'background-color:rgba(255,255,255,0.5)'
+					, 'background-color:rgba(255,255,255,0.5);'
+					, 'visibility:hidden;'
 					]).join('');
 				d.id = DOMID_PANEL;
 				_doc.body.appendChild(d);
@@ -38,10 +91,11 @@
 				cb.href = 'javascript:;';
 				cb.title = 'Click to close';
 				d.appendChild(cb);
+				addEvent(cb, 'click', hideViewportWindow);
 			}
 		}
 
-		exports.bootstrap = function(){
+		exports.bootstrap = function(allConsoleMethodMap, logConsoleMethodMap){
 				createViewportWindow();
 			};
 	});
