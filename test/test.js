93d654b82df2b3ffd7c12032b36566d852b9c1df
Shaw Hsu <scorrxu@gmail.com>
2013-05-16 00:45:56 +0800

    优化逻辑

--- test/test.js
+++ test/test.js
@@ -1,6 +1,26 @@
 define(function(retuire, exports, module){
 	function response(oData){
-		QZONE.console.log(oData);
+		var t = document.getElementById('txt_out')
+		, buff = []
+		;
+
+		if(oData && ('object' === typeof oData)){
+			if('[object Event]' === oData.toString()){
+				QZONE.console.error('error load JSONP, script onerror');
+				t && (t.innerHTML = buff.join('Error!'));
+			} else {
+				QZONE.console.info('error load JSONP succeed');
+				for(var k in oData){
+					buff.push(k + ' : ' + oData[k]);
+					QZONE.console.log('[', k, '] added to out stream');
+				}
+				t && (t.innerHTML = buff.join('<br />'));
+				QZONE.console.info('showout succeed');
+			}
+		} else {
+			QZONE.console.warn('error load JSONP, unknow error');
+			t && (t.innerHTML = buff.join('Error!'));
+		}
 	}
 
 	function start2ShowWeather(){
@@ -16,6 +36,7 @@ define(function(retuire, exports, module){
 			t = document.createElement('script');
 			t.type = 'text/javascript';
 			t.charset = 'utf-8';
+			t.onerror = response;
 			hd.appendChild(t);
 			t.src = 'http://r.qzone.qq.com/cgi-bin/user/qzone_msgtunnel_geoinfo';
 		}
