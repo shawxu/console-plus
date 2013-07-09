8349af79cba1062438d8969eed628413d2e40810
shawxu <sdscorpion@gmail.com>
2013-07-09 15:52:25 +0800

    确定是IE6/7才拉取展现面板模拟

--- src/console-plus.js
+++ src/console-plus.js
@@ -111,7 +111,8 @@
 				LOG_MAP[k] && (proto[k] = consoleFactory(k));
 			}
 
-			(require.async || require)(['./components/viewport'], function(vp){
+			//低版本IE才拉，用window.console无法完全判断没有console，这里单独判断IE6/7才拉
+			(_doc && _wnd && _wnd.ActiveXObject && !_doc.documentMode) && (require.async || require)(['./components/viewport'], function(vp){
 					vp.bootstrap(LOG_MAP);
 				});
 		}
