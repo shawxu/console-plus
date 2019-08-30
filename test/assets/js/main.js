7372bb663456b74306bd3e27741ff22302931139
shawxu <scorrxu@gmail.com>
2019-08-30 16:45:21 +0800

    console-plus ES6化基本整改完毕

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -1,10 +1,8 @@
-'use strict';
-
 requirejs.config({
   baseUrl: "assets/js",
   paths: {
     domReady: "./lib/requireJs/domReady",
-    consolePlus: "../../../src/console-plus"
+    consolePlus: "../../../src/console-plus" //直接引用src文件
   }
 });
 
@@ -17,8 +15,13 @@ require(['domReady', 'consolePlus'], (domReady, _cp) => {
     _cp.config({
       writeConsolePanel: true //让console-plus能把log打到控制台
     });
-    _cp.info("start");
+    _cp.info("info start");
+    _cp.debug("debug hahaha");
+    _cp.error("error lalala");
+    _cp.warn("warn wowowo");
+    _cp.log("log papapa");
 
+    document.querySelectorAll("#txt_out")[0].textContent = _cp.get();
   });
 });
 
